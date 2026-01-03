b1dee27f08e3a092fdcced61950c133653ea78f9
kietruru090 <kietruru090@gmail.com>
2026-01-03 12:18:41 +0700

    Add files via upload

--- WK/settings.py
+++ WK/settings.py
@@ -31,35 +31,31 @@ DASH_DURATION = 25
 DASH_COOLDOWN = 60   
 HEALTH_PICKUP_AMOUNT = 30
 TILE_SIZE = 50
-GATE_WIDTH = TILE_SIZE * 1.5
-GATE_HEIGHT = TILE_SIZE * 2.3
+GATE_WIDTH = TILE_SIZE * 1.8
+GATE_HEIGHT = TILE_SIZE * 2.6
  
 REQUIRED_KEYS = 3 
 
-# --- HỆ THỐNG LEVEL (ĐÃ SỬA LẠI LOGIC: DỄ LẤY CHÌA HƠN) ---
-# Quy tắc: 1 dòng trần, 1 dòng sàn.
-# Chìa khóa (K) được đặt trên bục hoặc độ cao vừa phải.
+#X: Đất F: Cổng kết thúc P: Người chơi W: Sói D: Hiệp sĩ bóng tối L: Trùm lửa K: Chìa khóa H: Bình máu A: Đạn (Ammo)
 
-# LEVEL 1: ĐỒNG CỎ (Khởi động nhẹ nhàng)
 LEVEL_0 = [
 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
 'X                                                                                                  X',
 'X                                                                                                  X',
+'X                                    K                                                             X',
 'X                                                                                                  X',
 'X                                                                                                  X',
 'X                                                                                                  X',
-'X                                                                                                  X',
-'X                                     K                                                            X',
-'X                                   XXXXX                                                          X',
-'X                 K               XX     XX                         K              E       F       X',
-'X P             XXXXX           XX         XX                     XXXXX          XXXXX   XXXXX     X',
-'XXXX          XX     XX       XX             XX                 XX     XX      XX     XXX     XX   X',
-'X           XX         XX   XX                 XX             XX         XX  XX                 XX X',
-'X      E  XX             XXX                     XX  E      XX             XXX                    XX',
+'X                               XX         XX                                                      X',
+'X                              XX         XX                                                       X',
+'X                             XX           XX                       K              W               X',
+'X            XXXXX           XX        H    XX                    XXXXX          XXXXX   XXXXX     X',
+'X           X               XX       XXX      XX                XX     XX      XX     XXX          X',
+'X           X              XX                  XX             XX         XXXXXX                   XX',
+'X P         X K       W   XX       W             XX  W      XX            XXXX                  F XX',
 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
 ]
 
-# LEVEL 2: KHÔNG GIAN (Nhảy qua các bục bay)
 LEVEL_1 = [
 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
 'X                                                                                                  X',
@@ -68,71 +64,68 @@ LEVEL_1 = [
 'X             K                                                                                    X',
 'X           XXXXX                                    K                                             X',
 'X                                                  XXXXX                          K                X',
-'X                                                                               XXXXX              X',
-'X                       E          H          E              A          E                    F     X',
-'X P                   XXXXX      XXXXX      XXXXX          XXXXX      XXXXX                XXXXX   X',
-'XXXX       XXXXX     XX   XX    XX   XX    XX   XX        XX   XX    XX   XX             XX     XX X',
-'X         XX   XX                                                                                  X',
-'X                                                                                                  X',
-'X                                                                                                  X',
-'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+'X                                                                      L  x     XXXXX      X       X',
+'X                                  H          W              A       XXXXXX                X F     X',
+'X P                   XXXXX      XXXXX      XXXXX          XXXXX                           XXXXX   X',
+'XXXXX     XX   X                                                                                XX X',
+'X         X     X                                                                                  X',
+'X         X     XXX                                                                                X',
+'X      L  X     X    W                         W                    W                              X',
+'XXXXXXXXXXX     XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
 ]
 
-# LEVEL 3: LEO NÚI (Chìa khóa ở trên đỉnh)
 LEVEL_2 = [
 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
 'X                                                                                                  X',
-'X                                                      K                    E                    F X',
-'X                                          E         XXXXX                XXXXX                XXXXX',
-'X                                        XXXXX     XX     XX            XX     XX            XX    X',
-'X                          K           XX     XX XX         XX        XX         XX        XX      X',
-'X                        XXXXX       XX                                            XX    XX        X',
-'X                      XX     XX   XX                                                XXXX          X',
-'X          E         XX         XXX            H             K             A                       X',
-'X P      XXXXX     XX                        XXXXX         XXXXX         XXXXX                     X',
-'XXXX   XX     XX XX                                                                                X',
-'X    XX                                                                                            X',
-'X                                                                                                  X',
-'X                                                                                                  X',
-'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+'X  P                                                   K                                         F X',
+'XXXXXXXXXXXXX                              W         XXXXX                XXXXX                XXXXX',
+'X           X                            XXXXX     XX     XX            XX     XX            XX    X',
+'X      K    X             H            XX     XXXXX         XX        XX         XX   D    XX      X',
+'X           X            XXXXX       XX                                            XX    XX        X',
+'X           X          XX     XX   XX                                                XXXX          X',
+'X        W  X       XXX         XXX            H             K             A                       X',
+'X       XXXXX     XX                         XXXXX         XXXXX         XXXXX                     X',
+'X      XX   X    X                                                                                 X',
+'X    XX     X   XXX                                  XXX                                XXXXX      X',
+'X                                                                                       X          X',
+'X                          W                                      L                     X       D  X',
+'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX     XXXXXX',
 ]
 
-# LEVEL 4: HẦM MỘ (Trần thấp, Chìa khóa giấu trong hốc)
 LEVEL_3 = [
 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
 'X                                                                                                  X',
-'X   P          K              H              K              A              K                     F X',
+'X   P                         H                             A              K                     F X',
 'X            XXXXX          XXXXX          XXXXX          XXXXX          XXXXX                 XXXXX',
 'X          XX     XX      XX     XX      XX     XX      XX     XX      XX     XX             XX    X',
-'XXXXXXXXXXX         XXXXXX         XXXXXX         XXXXXX         XXXXXX         XXXXXX     XX      X',
+'XXXXXXXXXXX                        XXXXXX                        XXXXXX         XXXXXX     XX      X',
+'X                                                                                                  X',
+'X                                                    XXX                                           X',
+'X   K W           H                 W                             W              D                 X',
+'X  XXXXXXX        XXXXXXX        XXXXXXX          K            XXXXXXX        XXXXXXX              X',
+'X                                                XXX                                               X',
 'X                                                                                                  X',
 'X                                                                                                  X',
-'X     E              E              E              E              E              E                 X',
-'X  XXXXXXX        XXXXXXX        XXXXXXX        XXXXXXX        XXXXXXX        XXXXXXX              X',
-'X                                                                                                  X',
-'X                                                                                                  X',
-'X                                                                                                  X',
-'X                                                                                                  X',
-'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+'X                           X    X                       X    X                                    X',
+'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX    XXXXXXXXXXXXXXXXXXXXXXXXX    XXXXXXXXXXXXXXXXXXXXXXXX     XXXXXXXXX',
 ]
 
-# LEVEL 5: HỖN HỢP (Khó nhất - Vực sâu)
 LEVEL_4 = [
 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
 'X                                                                                                  X',
-'X                                                                                                  X',
-'X                                                                                        E       F X',
-'X               K                             K               E            K           XXXXX   XXXXX',
-'X             XXXXX           E             XXXXX           XXXXX        XXXXX        XX   XX XX   X',
-'X                           XXXXX                         XX     XX    XX     XX     XX     XXX    X',
-'X     E                   XX     XX                     XX         XXXX         XX                 X',
-'X P XXXXX               XX         XX                 XX                          XX               X',
-'XXXX     XX           XX             XX             XX                              XX             X',
-'X          XX       XX                 XX         XX                                  XX           X',
-'X            XXXXXXX                     XXXXXXXXX                                      XXXXXXXX   X',
-'X                                                                                                  X',
-'X                                                                                                  X',
-'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+'X      K                                                                                           X',
+'X                                                                                                F X',
+'X              L                             K               D            K            XXXXX   XXXXX',
+'X             XXXXX           W             XXXXX           XXXXX        XXXXX        XX   XX XX   X',
+'X                           XXXXX                         XX     XX    XX     XX      X     XXX   KX',
+'X                         XX     XX                     XX         XXXX        XX                  X',
+'X P  XXXX               XX         XX                 XX                         XXX               X',
+'XXXXX    XX           XX             XX             XX       H                      XX             X',
+'X          XX       XX       H         XX         XX                 A                XX           X',
+'X            XXXXXXX                     XXX   XXX                                      XXXXXX     X',
+'X      K                                                                                           X',
+'X D                  D          W                            L                                     X',
+'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX    XXXXXXXXXXXXXXXXXX',
 ]
 
 ALL_LEVELS = [LEVEL_0, LEVEL_1, LEVEL_2, LEVEL_3, LEVEL_4]
--- WK/tile.py
+++ WK/tile.py
@@ -1,79 +1,51 @@
 # tile.py
 import pygame
 import os
-from settings import * # Đảm bảo đã import GATE_WIDTH, GATE_HEIGHT
+from settings import *
 
-# --- CLASS ĐẤT (Giữ nguyên không đổi) ---
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, size):
         super().__init__()
         base_path = os.path.dirname(__file__)
         img_path = os.path.join(base_path, 'assets', 'ground.png')
         
-        try:
-            original_image = pygame.image.load(img_path).convert_alpha()
-            self.image = pygame.transform.scale(original_image, (size, size))
-        except Exception as e:
-            self.image = pygame.Surface((size, size))
-            self.image.fill(BROWN)
-            pygame.draw.rect(self.image, GREEN, (0, 0, size, 5))
+        # Bắt buộc load ảnh
+        original_image = pygame.image.load(img_path).convert_alpha()
+        self.image = pygame.transform.scale(original_image, (size, size))
         
         self.rect = self.image.get_rect(topleft=pos)
 
     def update(self, x_shift):
         self.rect.x += x_shift
 
-# --- CẬP NHẬT CLASS CỔNG ĐÍCH (TO HƠN) ---
 class FinishLine(pygame.sprite.Sprite):
     def __init__(self, pos, tile_size):
-        # pos: là tọa độ góc trên-trái của ô đất chứa chữ 'F' (kích thước 50x50)
         super().__init__()
         self.is_locked = True
         base_path = os.path.dirname(__file__)
         
-        # --- 1. TẢI ẢNH VỚI KÍCH THƯỚC MỚI (TO HƠN) ---
+        target_size = (GATE_WIDTH, GATE_HEIGHT)
         locked_path = os.path.join(base_path, 'assets', 'gate_locked.png')
         open_path = os.path.join(base_path, 'assets', 'gate_open.png')
+
+        # Bắt buộc load ảnh
+        img_locked = pygame.image.load(locked_path).convert_alpha()
+        self.locked_image = pygame.transform.scale(img_locked, target_size)
         
-        # Sử dụng GATE_WIDTH và GATE_HEIGHT từ settings.py
-        target_size = (GATE_WIDTH, GATE_HEIGHT)
+        img_open = pygame.image.load(open_path).convert_alpha()
+        self.open_image = pygame.transform.scale(img_open, target_size)
 
-        try:
-            img_locked = pygame.image.load(locked_path).convert_alpha()
-            self.locked_image = pygame.transform.scale(img_locked, target_size)
-            
-            img_open = pygame.image.load(open_path).convert_alpha()
-            self.open_image = pygame.transform.scale(img_open, target_size)
-            
-        except Exception as e:
-            print(f"Lỗi tải ảnh cổng: {e}. Dùng hình khối thay thế.")
-            self.locked_image = pygame.Surface(target_size)
-            self.locked_image.fill(RED)
-            self.open_image = pygame.Surface(target_size)
-            self.open_image.fill((255, 215, 0)) 
-
-        # --- 2. THIẾT LẬP VỊ TRÍ (QUAN TRỌNG) ---
         self.image = self.locked_image
         self.rect = self.image.get_rect()
-
-        # Logic căn chỉnh:
-        # 'pos' là góc trên-trái của ô 'F' nhỏ (50x50).
-        # Chúng ta muốn chân của cổng to nằm ở đáy của ô 'F' đó.
         
-        # Tọa độ Y của mặt đất tại ô 'F'
         ground_y = pos[1] + tile_size 
-        
-        # Đặt đáy của cổng to trùng với mặt đất
         self.rect.bottom = ground_y
-        # Đặt cạnh trái của cổng trùng với cạnh trái ô 'F'
         self.rect.left = pos[0]
 
     def unlock(self):
-        """Hàm mở khóa cổng"""
         if self.is_locked:
             self.is_locked = False
             self.image = self.open_image
-            # Cập nhật lại rect để đảm bảo vị trí không đổi sau khi đổi ảnh
             old_bottomleft = self.rect.bottomleft
             self.rect = self.image.get_rect()
             self.rect.bottomleft = old_bottomleft
